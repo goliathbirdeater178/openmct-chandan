102152ebc4434c70992b0dd30e9eadf64a9fb937
Chandan Hegde <hegde.chan@gmail.com>
2017-04-14 15:59:53 -0700

    Defined root for custom plugin

--- chandan-custom-plugin.js
+++ chandan-custom-plugin.js
@@ -1,5 +1,12 @@
 function ChandanCustomPlugin() {
     return function install() {
         console.log("Chandan Custom Plugin is installed!");
+		//Adds root to OpenMCT tree
+		openmct.addRoot({
+			namespace: "custom-namespace",
+			key: "custom-key"
+		});
     }
 };
+
+
